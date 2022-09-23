75130fb1e40005c8cba710e817cb4bf5cce02f9b
grohovski_igor <i.grokhovskij@dmaby.com>
2022-09-23 21:24:50 +0300

    Обновляет скрипт для установки node js

--- provisions/node_load.sh
+++ provisions/node_load.sh
@@ -5,6 +5,20 @@ function installNodeJs() {
 
   npm install npm@latest -g
   npm install -g create-react-app
+
+  sudo apt install build-essential checkinstall
+  sudo apt install libssl-dev
+
+  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+  source /etc/profile
+
+  echo "Смотрим доступные версии: "
+
+  nvm ls-remote
+
+  nvm install 16.17.1
+  nvm use 16.17.1
 }
 
 installNodeJs
